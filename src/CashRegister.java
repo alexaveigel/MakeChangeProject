19f4e82b36937e4ed8f85a537a2fc2a657022b84
aveigel <alexaveigel@yahoo.com>
2019-03-15 15:57:46 -0700

    Update CashRegister thru User Story 3

--- src/CashRegister.java
+++ src/CashRegister.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
 
 public class CashRegister {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Please enter the price of your item >> ");
+		double itemPrice = sc.nextDouble();
+	
+		System.out.println("Pleade enter how much money are you inserting >> ");
+		double moneyInserted = sc.nextDouble();
+		
+		if (moneyInserted == itemPrice) {
+			System.out.println("Thank you for exact change! ");
+		}
+		else if (moneyInserted < itemPrice) {
+			System.out.println("That is not enough money :( ");
+		}
+
+		sc.close();
 	}
 
 }
