96f39d342f3c46c8f6f8e5c8d1837226af3ceb88
aveigel <alexaveigel@yahoo.com>
2019-03-16 10:12:24 -0700

    MVP upload

--- src/CashRegister.java
+++ src/CashRegister.java
@@ -5,13 +5,6 @@ public class CashRegister {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		double bill10 = 10;
-		double bill5 = 5;
-		double bill1 = 1;
-		double coin25 = 0.25;
-		double coin10 = 0.10;
-		double coin05 = 0.05;
-		double coin01 = 0.01;
 		int bill10Count = 0;
 		int bill5Count = 0;
 		int bill1Count = 0;
@@ -19,8 +12,6 @@ public class CashRegister {
 		int coin10Count = 0;
 		int coin05Count = 0;
 		int coin01Count = 0;
-		
-		
 
 		System.out.println("Please enter the price of your item >> ");
 		double itemPrice = sc.nextDouble();
@@ -28,7 +19,7 @@ public class CashRegister {
 		System.out.println("Please enter how much money are you inserting >> ");
 		double moneyInserted = sc.nextDouble();
 		double change = moneyInserted - itemPrice;
-		
+
 		System.out.println("Your change is >> " + (moneyInserted - itemPrice));
 
 		if (moneyInserted == itemPrice) {
@@ -36,60 +27,60 @@ public class CashRegister {
 		} else if (moneyInserted < itemPrice) {
 			System.out.println("That is not enough money :( ");
 		}
-		
-		while(change >= 10) {
+
+		while (change >= 10) {
 			bill10Count++;
 			change = change - 10;
-		
+
 		}
-		while(change >= 5) {
+		while (change >= 5) {
 			bill5Count++;
 			change = change - 5;
 		}
-		while( change >= 1) {
+		while (change >= 1) {
 			bill1Count++;
 			change = change - 1;
 		}
-		while( change >= 0.25) {
+		while (change >= 0.25) {
 			coin25Count++;
 			change = change - 0.25;
 		}
-		while( change >= 0.10) {
+		while (change >= 0.10) {
 			coin10Count++;
 			change = change - 0.10;
 		}
-		while( change >= 0.05) {
+		while (change >= 0.05) {
 			coin05Count++;
 			change = change - 0.05;
 		}
-		while( change >= 0.01) {
+		while (change >= 0.01) {
 			coin01Count++;
 			change = change - 0.01;
 		}
-		
-			
+		if (bill10Count > 0) {
+			System.out.println("You get one 10 dollar bill");
+		}
+		if (bill5Count > 0) {
+			System.out.println("You get one 5 dollar bill");
+		}
+		if (bill1Count > 0) {
+			System.out.println("You get " + bill1Count + " dollar bills");
+		}
+		if (coin25Count > 0) {
+			System.out.println("You get " + coin25Count + " quarters");
+		}
 
-//		else if ((moneyInserted - itemPrice) > 10) {
-//			System.out.println("You get one 10 dollar bill");
-//			double change = ((moneyInserted - itemPrice) - 10);
-//
-//		}
-//
-//		else if (change > 5) {
-//
-//			System.out.println("You get one 5 dollar bill");
-//
-//		}
-//		else if ((moneyInserted - itemPrice) > 1) {
-//			System.out.println("You get one  dollar bill"); 
-//		}
-//		
-//		
-//		else if (moneyInserted  )
-////	
-//			do { 
-//				
-//		}
+		if (coin10Count > 0) {
+			System.out.println("You get " + coin10Count + " dimes");
+		}
+
+		if (coin05Count > 0) {
+			System.out.println("You get " + coin05Count + " nickles");
+		}
+
+		if (coin01Count > 0) {
+			System.out.println("You get " + coin01Count + " pennies");
+		}
 
 		sc.close();
 	}
